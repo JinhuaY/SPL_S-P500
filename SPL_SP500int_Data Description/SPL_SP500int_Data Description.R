a50cbf4f43a6b8d0df12ac04fd29b5a5b66270c3
JinhuaY <yangjinh@cms.hu-berlin.de>
2016-08-11 12:07:44 +0200

    Update SPL_SP500int_Data Description.R

--- SPL_SP500int_Data Description/SPL_SP500int_Data Description.R
+++ SPL_SP500int_Data Description/SPL_SP500int_Data Description.R
@@ -5,28 +5,34 @@ graphics.off()
 # set working directory 
 # setwd('...')
 
+# Install packages if not installed
+libraries = c("stargazer", "ggplot2", "gplots")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+
+# Load packages
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+
+#read data
 Paneldata = read.csv("Panel.csv")
-# *data description analysis *plot graphics (use different color for different companies)
+
+# data description analysis
+# plot graphics (use different color for different companies)
 Ri = Paneldata$Ri
 Rm = Paneldata$Rm
 summary(Paneldata$Ri)
 summary(Paneldata$Rm)
 
-# install.packages('stargazer')
-library("stargazer")
 stargazer(Paneldata)
 
-# install.packages('ggplot2')
-library(ggplot2)
 p = ggplot(Paneldata, aes(x = Rm,y = Ri,colours = factor(company)))
 p + geom_point(aes(colour = factor(company))) + stat_smooth()
 
-# install.packages('gplots')
-library(gplots)
 plotmeans(Ri ~ company, main = "Heterogeineity across companies", data = Paneldata)
 plotmeans(Ri ~ date, n.label = FALSE, main = "Heterogeneity across time", data = Paneldata)
 
-# *Histograms with norm density use hist() for market price
+# Histograms with norm density use hist() for market price
 h = hist(Ri, breaks = 10, col = "blue")
 xfit = seq(min(Ri), max(Ri), length = 100)
 yfit = dnorm(xfit, mean = mean(Ri), sd = sd(Ri))
@@ -38,4 +44,4 @@ box()
 p = ggplot(Paneldata, aes(x = Ri))
 p + geom_histogram(aes(fill = factor(company), y = ..density..), alpha = 0.3, colour = "blue") + 
     stat_density(geom = "line", position = "identity", size = 1.5, aes(colour = factor(company))) + 
-    facet_wrap(~company, ncol = 1)
+    facet_wrap(~company, ncol = 1) 
