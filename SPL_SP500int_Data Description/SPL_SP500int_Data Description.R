571efbbd3ad91d633127346b1ce2d870ea83e338
zhangwey333 <zhangwey333@users.noreply.github.com>
2016-08-14 17:00:26 +0800

    Update SPL_SP500int_Data Description.R

--- SPL_SP500int_Data Description/SPL_SP500int_Data Description.R
+++ SPL_SP500int_Data Description/SPL_SP500int_Data Description.R
@@ -31,17 +31,3 @@ p + geom_point(aes(colour = factor(company))) + stat_smooth()
 
 plotmeans(Ri ~ company, main = "Heterogeineity across companies", data = Paneldata)
 plotmeans(Ri ~ date, n.label = FALSE, main = "Heterogeneity across time", data = Paneldata)
-
-# Histograms with norm density use hist() for market price
-h = hist(Ri, breaks = 10, col = "blue")
-xfit = seq(min(Ri), max(Ri), length = 100)
-yfit = dnorm(xfit, mean = mean(Ri), sd = sd(Ri))
-yfit = yfit * diff(h$mids[1:2]) * length(Ri)
-lines(xfit, yfit, col = "red", lwd = 2)
-box()
-
-# use ggplot2 for individual company
-p = ggplot(Paneldata, aes(x = Ri))
-p + geom_histogram(aes(fill = factor(company), y = ..density..), alpha = 0.3, colour = "blue") + 
-    stat_density(geom = "line", position = "identity", size = 1.5, aes(colour = factor(company))) + 
-    facet_wrap(~company, ncol = 1) 
